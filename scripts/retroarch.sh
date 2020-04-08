914c56ef309598d0f819c033a59baf0a388e387f
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-04-08 11:00:43 +0200

    Add script to install RetroArch

---
+++ scripts/retroarch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Description : RetroArch
+# Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
+# Version     : 1.0 (Apr/20)
+# Compatible  : Raspberry Pi 4 (tested)
+#
+clear
+. ./helper.sh
+check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
+
+function install() {
+	echo 'Installing Retroarch...'
+	# TODO Check if package is already installed
+	apk add --upgrade retroarch
+}
+
+install_X11
+install
