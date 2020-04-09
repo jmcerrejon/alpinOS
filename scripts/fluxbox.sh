a020f338c562a7ee0c4f00f2c8fbe01debe35dc7
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-04-09 09:57:31 +0200

    Refactor code

--- scripts/fluxbox.sh
+++ scripts/fluxbox.sh
@@ -8,7 +8,7 @@
 #		  http://www.linuxfromscratch.org/blfs/view/svn/x/fluxbox.html
 #
 clear
-. ./helper.sh
+. ./helper.sh || wget -q 'https://raw.githubusercontent.com/jmcerrejon/alpinOS/master/scripts/helper.sh'
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 function install() {
--- scripts/retroarch.sh
+++ scripts/retroarch.sh
@@ -6,7 +6,7 @@
 # Compatible  : Raspberry Pi 4 (tested)
 #
 clear
-. ./helper.sh
+. ./helper.sh || wget -q 'https://raw.githubusercontent.com/jmcerrejon/alpinOS/master/scripts/helper.sh'
 check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
 
 function install() {
