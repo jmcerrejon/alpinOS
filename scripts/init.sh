6fa09fbdca544dfdc1e532f8ef1f6b96d8c46c45
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-04-10 12:55:31 +0200

    Fix issue with dload script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ash
 #
 # Description : Init - Just some code to run when you start from scratch
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
@@ -6,7 +6,8 @@
 # Compatible  : Raspberry Pi 4 (tested)
 #
 clear
-. ./helper.sh || wget -q 'https://raw.githubusercontent.com/jmcerrejon/alpinOS/master/scripts/helper.sh'
+wget -q 'https://raw.githubusercontent.com/jmcerrejon/alpinOS/master/scripts/helper.sh'
+. ./helper.sh
 
 # Enable edge & community repositories
 enable_repos
@@ -15,3 +16,7 @@ enable_repos
 install_common_pkgs
 
 add_pi_user
+
+git clone https://github.com/jmcerrejon/alpinOS.git
+
+echo -e '\nSystem is ready!...Have a nice day.\n'
